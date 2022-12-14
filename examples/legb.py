255decb0eee9b9b7cd878e31225b0c169ffdeb71
Iulia Chiriac <iulia.chiriac@yahoo.com>
2022-12-14 12:44:07 +0200

    Add LEGB example

---
+++ examples/legb.py
@@ -0,0 +1,35 @@
+X = 100
+
+
+def my_func(a):
+    b = 10
+
+    # print('id X inside function before shadowing', id(X))
+    X = "ceva"
+    # print('id X inside function after shadowing', id(X))
+
+    def inner(c):
+        d = 11
+
+        print("Built-in names used in local (inner) scope:", int, len)
+        print("Global names used in local (inner) scope:", X, my_func, MyClass)
+        print("Enclosing names used in local (inner) scope:", a, b, inner)
+        print("Local names used in local (inner) scope:", c, d, end="\n\n")
+
+    inner(22)
+
+    print("Built-in names used in local (my_func) scope:", int, ValueError, len)
+    print("Global names used in local (my_func) scope:", X, my_func, MyClass)
+    print("Local names used in local (my_func) scope:", a, b, inner, end="\n\n")
+
+
+class MyClass:
+    pass
+
+
+# print('id X global before function call', id(X))
+my_func(20)
+# print('id X global before after call', id(X))
+
+print("Built-in names used in global scope:", int, ValueError, len)
+print("Global names used in global scope:", X, my_func, MyClass)
